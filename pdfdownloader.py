cf0272b307dd512cb0daf2ad04828ca0d5dd97f6
Yousif Toama <30576348+yousif-toama@users.noreply.github.com>
2021-01-03 03:29:33 +0000

    Streamlined list generators.

--- pdfdownloader.py
+++ pdfdownloader.py
@@ -15,39 +15,21 @@ def get_links(url):
     response = requests.get(url)
     data = response.text
     soup = BeautifulSoup(data, 'lxml')  # requires lxml module
-
-    links = []
-    for link in soup.find_all('a'):
-        link_url = link.get('href')
-
-        if link_url is not None:
-            links.append(link_url + '\n')  # link formatting
-
-    return links
+    return [x.get('href') + '\n' for x in soup.find_all('a') if x.get('href') is not None]
 
 
 result = get_links(target_url)
-filtered = []
-
 # filter for links containing 'node' (i.e. the ones containing resources and filtering out other linked websites)
-for i in result:
-    if 'node' in i:
-        filtered.append(i[:-1])
+filtered = [i[:-1] for i in result if 'node' in i]
 
-# list of links to course materials pages
-course_materials = []
-for i in filtered:
-    course_materials.append('https://courses.maths.ox.ac.uk' + i + '/materials')  # turns paths into links
+# list of links to course materials pages (turns paths into links)
+course_materials = ['https://courses.maths.ox.ac.uk' + i + '/materials' for i in filtered]
 
 # go through each course_materials link
 for i in course_materials:
-    # get links on course materials page
-    cm_links = get_links(i)
+    cm_links = get_links(i)  # get links on course materials page
     # get all links with 'download' in them
-    download = []
-    for j in cm_links:
-        if 'download' in j:
-            download.append('https://courses.maths.ox.ac.uk' + j[:-1])
+    download = ['https://courses.maths.ox.ac.uk' + j[:-1] for j in cm_links if 'download' in j]
     for j in download:
         r = requests.get(j)
         with open(j[-5:] + '.zip', 'wb') as f:  # download the files
